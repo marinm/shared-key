e6c7e747b02b4a931df5f8a71c26406446de8471
Marin <marinmema@live.ca>
2025-02-01 10:21:55 -0500

    validate friend code before trying to connect

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ function showScreen(screenId) {
         .querySelectorAll(".screen")
         .forEach((el) => el.classList.add("hidden"));
 
-	const delay = (screenId === currentScreenId) ? 0 : 200;
+    const delay = screenId === currentScreenId ? 0 : 200;
 
-	setTimeout(() => {
-		document.getElementById(screenId).classList.remove("hidden");
-	}, delay);
+    setTimeout(() => {
+        document.getElementById(screenId).classList.remove("hidden");
+    }, delay);
 }
 
 function renderOwnCode() {
-    [...ownCode].forEach((value, index) => {
+    ownCode.forEach((value, index) => {
         const digit = document.querySelector(
             `#own-code .digit[data-position="${index}"]`
         );
@@ -79,11 +79,52 @@ document.querySelectorAll(".keypad-btn").forEach((element) => {
 });
 
 document.getElementById("connect-btn").addEventListener("click", (event) => {
-    showScreen("screen-waiting");
+    connect();
 });
 
 function randomCode() {
     const array = new Uint32Array(1);
     self.crypto.getRandomValues(array);
-    return String(array[0]).padStart(10, "0").slice(-CODE_LENGTH);
+    return String(array[0]).padStart(10, "0").slice(-CODE_LENGTH).split("");
+}
+
+function validateFriendCode() {
+    const isValid = friendCode.every((d) => "0123456789".includes(d));
+
+    if (isValid) {
+        return true;
+    }
+
+    flashMissingDigits();
+
+    return false;
+}
+
+function friendCodeDigitAt(position) {
+	return document.querySelector(`#friend-code .digit[data-position="${position}"]`);
+}
+
+function flashMissingDigits() {
+    const digits = [...document.querySelectorAll("#friend-code .digit")];
+
+	friendCode.forEach((d, index) => {
+		if (!"0123456789".includes(d)) {
+			friendCodeDigitAt(index).classList.add('focus');
+		}
+	})
+
+    const wait = 500;
+
+    setTimeout(() => renderFocus(), wait);
+}
+
+function connect() {
+    const tempSharedKey = friendCode.join("") + ownCode.join("");
+    console.log(tempSharedKey);
+
+    if (!validateFriendCode()) {
+        return;
+    }
+
+    showScreen("screen-waiting");
 }
